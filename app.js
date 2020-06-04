3b4159819afb63c972a58a0e4eed70b1be672e28
ignatiusandrew <reachignatius@gmail.com>
2020-06-04 16:59:46 +0530

    changet to React app

--- app.js
+++
@@ -1,7 +0,0 @@
-const express = require ('express');
-const app = express();
-
-app.get('/', (req, res)=>res.send("hello igi welcome to the new world of devops"));
-app.listen(3000,()=>{
-    console.log("igi");
-})  
